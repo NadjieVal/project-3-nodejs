ae1226bf3c5c6e57367ad91b91510692f6746be3
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-02-27 18:35:04 +0100

    setting app.js and creating files in bin config and routes. deleting useless fileslike hbs  and folders like views

---
+++ routes/auth-router.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user-model.js");
+
+const router = express.Router();
+
+router.post("/process-signup", (req, res, next) => {
+  const { firstName, lastName, email, originalPassword } = req.body;
+
+  if (!originalPassword || !originalPassword.match(/[0-9]/)) {
+    next(new Error("Password can't be blank and must contain a number."));
+    return;
+  }
+
+  const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
+
+  User.create({ firstName, lastName, email, encryptedPassword })
+    .then(userDoc => {
+      req.logIn(userDoc, () => {
+        userDoc.encryptedPassword = undefined;
+        res.json(userDoc);
+      });
+    })
+    .catch(err => next(err));
+});
+
+router.post("/process-login", (req, res, next) => {
+  const { email, originalPassword } = req.body;
+
+  User.findOne({ email: { $eq: email } })
+    .then(userDoc => {
+      if (!userDoc) {
+        next(new Error("Email is incorrect."));
+        return;
+      }
+
+      const { encryptedPassword } = userDoc;
+
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        next(new Error("Password is incorrect."));
+        return;
+      }
+
+      req.logIn(userDoc, () => {
+        userDoc.encryptedPassword = undefined;
+        res.json(userDoc);
+      });
+    })
+    .catch(err => next(err));
+});
+
+router.get("/logout", (req, res, next) => {
+  req.logOut();
+  res.json({ message: "You are logged out!" });
+});
+
+module.exports = router;
