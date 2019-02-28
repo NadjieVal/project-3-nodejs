b730bbd1ae50acb62656d6c58d39800ca2d56e2b
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-02-28 11:31:53 +0100

    created charitySchema

--- models/charity-model.js
+++ models/charity-model.js
@@ -3,7 +3,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const charitySchema = new Schema(
-  {},
+  {
+    missionName: { type: String, required: true, minlength: 2 },
+    missionDescription: { type: String, required: true, minlength: 10 },
+    charityName: { type: String, required: true, minlength: 2 },
+    charityLogo: { type: String, required: true, match: /^https?:\/\// },
+    charityUrl: { type: String, required: true, match: /^https?:\/\// },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    location: { type: String, required: true },
+    category: {
+      type: String,
+      enum: [
+        "refugees",
+        "women",
+        "children",
+        "medicine",
+        "human-rights",
+        "animals"
+      ]
+    },
+    charityDescription: { type: String, required: true, minlength: 10 }
+  },
   {
     timestamps: true
   }
