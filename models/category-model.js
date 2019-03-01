92e928b97cf3a9bf35eee920193389d3222f710f
wowfranzik <7francescagatti@gmail.com>
2019-03-01 17:45:52 +0100

    fix category-model

--- models/category-model.js
+++ models/category-model.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema(
   {
     name: { type: String, required: true, minlength: 2 },
-    icon: { type: String, required: true, match: /^https?:\/\// }
+    icon: { type: String, required: true }
   },
   {
     timestamps: true
