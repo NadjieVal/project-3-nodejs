b1f633f1f36bc37b3522e003156878613206a7ce
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-03-01 13:37:13 +0100

    replace app name by timefor

--- config/nodemailer-setup.js
+++ config/nodemailer-setup.js
@@ -10,22 +10,22 @@ const transport = nodemailer.createTransport({
 
 function sendSignupMail(userDoc) {
   transport.sendMail({
-    from: "App Name <appName@example.com>",
+    from: "TimeFor <timefor@example.com>",
     to: ` ${userDoc.fullName} <${userDoc.email}>`,
     subject: "Thank you for ...",
     text: `Welcome, ${
       userDoc.firstName
-    }! Thank you for joining the App Name family.`,
+    }! Thank you for joining the TimeFor family.`,
     html: `
       <h1 style="color:orange;">Welcome, ${userDoc.fullName}!</h1>
-      <p style="font-style: italic;">Thank you for joining the App Name family</p>
+      <p style="font-style: italic;">Thank you for joining the TimeFor family</p>
       `
   });
 }
 
 function sendPasswordEmail() {
   return transport.sendMail({
-    from: "App Name <appName@example.com>",
+    from: "TimeFor <timefor@example.com>",
     to: ` ${userDoc.fullName} <${userDoc.email}>`,
     subject: "Don't forget your password",
     text: "Don't forget your password",
