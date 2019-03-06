0cb8d27ada4643339a8a2430e47a12681b9569a6
Poeli Bojorquez <pbojorquez@poeli.com>
2019-03-06 11:06:05 +0100

    merge conflicts

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -6,6 +6,12 @@ const User = require("../models/user-model.js");
 
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+  User.find()
+    .then(user => res.json(user))
+    .catch(err => err);
+});
+
 router.post("/process-signup", (req, res, next) => {
   const { firstName, lastName, email, originalPassword } = req.body;
 
@@ -56,6 +62,15 @@ router.post("/process-login", (req, res, next) => {
 router.get("/logout", (req, res, next) => {
   req.logOut();
   res.json({ message: "You are logged out!" });
+});
+
+router.get("/profile/:userId", (req, res, next) => {
+  const { userId } = req.params;
+  const { firstName, lastName, email } = req.body;
+  console.log("USERBACKEND", userId);
+  User.findById(userId)
+    .then(userDoc => res.json(userDoc))
+    .catch(err => next(err));
 });
 
 module.exports = router;
