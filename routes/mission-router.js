be8bccabd3312fdd34fac8cbdcf4487ab48f2007
Nadjie Valentin <nadjie.valentin@gmail.com>
2019-03-07 13:34:59 +0100

    added a create and a get in mission router

---
+++ routes/mission-router.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const Mission = require("../models/mission-model.js");
+
+const router = express.Router();
+const moment = require("moment");
+
+// we get the category linked to time in the DB
+router.post("/your-missions", (req, res, next) => {
+  const { charityLogo, charityName, missionName } = req.body;
+
+  Mission.create({ charityLogo, charityName, missionName, user: req.user._id })
+    .then(missionDoc => res.json(missionDoc))
+    .catch(err => next(err));
+});
+
+router.get("/your-missions", (req, res, next) => {
+  Mission.find({
+    user: { $eq: req.user._id }
+  })
+    .sort({ createdAt: -1 })
+    .then(missionResult => res.json(missionResult))
+    .catch(err => next(err));
+});
+
+module.exports = router;
